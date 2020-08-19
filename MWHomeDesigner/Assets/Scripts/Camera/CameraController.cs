2c98ac9f51c4830b74e7173fe9a4748e50169a1d
MrKuay <putzmaximilian@aol.com>
2020-08-19 17:14:35 +0200

    Camera Control Script
    Rotation and Zoom with and without button control

---
+++ MWHomeDesigner/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //pivot point which the camera will pivot around
+    [SerializeField] Transform rotationController;
+
+    [SerializeField] Camera camera;
+
+    //the strength the mouse input will influence the camera rotation
+    public float mouseSensitivity = 100f;
+    [Tooltip("Takes values (-1,1)")]
+    public int mouseDirection = 1;
+    //the strength the mouse input will influence the camera field of view (zoom in and out)
+    public float scrollSensitivity = 1f;
+    [Tooltip("Takes values (-1,1)\n-1: scroll up > zoom in\n 1: scroll up > zoom out")]
+    public int scrollDirection = 1;
+
+    //states for button controls
+    public bool buttonRotation = false;
+    public bool buttonZoom = false;
+    public bool buttonPan = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checking if any of the assistance buttons is pressed
+        if (Input.GetKey(KeyCode.Mouse0) && buttonRotation)
+        {
+            //rotation the pivot point which the camera is parented to around the y-axis
+            rotationController.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivity * mouseDirection * Time.deltaTime);
+        } else if(Input.GetKey(KeyCode.Mouse0) && buttonZoom)
+        {
+            camera.fieldOfView += Input.GetAxis("Mouse X") * mouseSensitivity * mouseDirection * Time.deltaTime;
+        } else if(Input.GetKey(KeyCode.Mouse0) && buttonPan)
+        {
+
+        } else if(Input.GetKey(KeyCode.Mouse0))
+        {
+            rotationController.Rotate(Vector3.up * Input.GetAxis("Mouse X") *mouseDirection * mouseSensitivity * Time.deltaTime);
+        }
+
+        //ZOOM
+        // adjusting the field of view to the mouse scroll input (makes zoom effect)
+        camera.fieldOfView += Input.mouseScrollDelta.y * scrollSensitivity * scrollDirection;
+
+        //PANING
+
+    }
+
+    //Setter for button controls
+    public void setButtonRotation(bool state)
+    {
+        buttonRotation = state;
+    }
+    public void setButtonZoom(bool state)
+    {
+        buttonZoom = state;
+    }
+    public void setButtonPan(bool state)
+    {
+        buttonPan = state;
+    }
+
+    //other setters
+    public void setScrollDirection(int value)
+    {
+        scrollDirection = value;
+    }
+}
