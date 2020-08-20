c250d136398b1c607833259323143a6fb99d7e0b
Leon <lportius@hs-mittweida.de>
2020-08-20 15:58:57 +0200

    go from intro menu to game scene

---
+++ MWHomeDesigner/Assets/Scripts/UI-Functionality/IntroMenuUIFunctionality.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroMenuUIFunctionality : MonoBehaviour
+{
+    public void loadScene(int levelToLoad)
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
