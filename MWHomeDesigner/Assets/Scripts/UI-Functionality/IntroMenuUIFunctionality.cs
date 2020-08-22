13ac18cfaa3c269966be8f770d1f65c897a18b14
MrKuay <putzmaximilian@aol.com>
2020-08-22 13:27:32 +0200

    Side Menu UI BG working
    open and close side menu, no functions in it

--- MWHomeDesigner/Assets/Scripts/UI-Functionality/IntroMenuUIFunctionality.cs
+++ MWHomeDesigner/Assets/Scripts/UI-Functionality/IntroMenuUIFunctionality.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class IntroMenuUIFunctionality : MonoBehaviour
 {
+    [SerializeField] GameObject menuUI;
+    [SerializeField] GameObject menuButton;
+    [SerializeField] GameObject menuCloseButton;
     public void loadScene(int levelToLoad)
     {
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void toggleMenu()
+    {
+        if (menuUI.activeSelf)
+        {
+            menuButton.SetActive(true);
+            menuCloseButton.SetActive(false);
+            menuUI.SetActive(false);
+        }
+        else
+        {
+            menuButton.SetActive(false);
+            menuCloseButton.SetActive(true);
+            menuUI.SetActive(true);
+        }
     }
 }
