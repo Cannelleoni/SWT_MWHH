05333bca5749fc36dc8c35a17d4cf0f461c07769
MrKuay <putzmaximilian@aol.com>
2020-08-22 21:59:35 +0200

    UI adjusts, Quit Button

--- MWHomeDesigner/Assets/Scripts/UI-Functionality/IntroMenuUIFunctionality.cs
+++ MWHomeDesigner/Assets/Scripts/UI-Functionality/IntroMenuUIFunctionality.cs
@@ -28,4 +28,9 @@ public class IntroMenuUIFunctionality : MonoBehaviour
             menuUI.SetActive(true);
         }
     }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
 }
