4de2ab98a6dab26195d1f51c25d986188f9ee74c
MrKuay <putzmaximilian@aol.com>
2020-09-01 12:47:57 +0200

    now displaying previed before placing object

--- MWHomeDesigner/Assets/Scripts/Grid/FurniturePlacementManager.cs
+++ MWHomeDesigner/Assets/Scripts/Grid/FurniturePlacementManager.cs
@@ -8,15 +8,31 @@ public class FurniturePlacementManager : MonoBehaviour
 {
     public static string activeFurniture;
 
+    public Transform lastPreviewed;
+    public GameObject previewfurniture;
+
     RaycastHit hit;
 
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) && !string.IsNullOrEmpty(activeFurniture))
+        if (!string.IsNullOrEmpty(activeFurniture))
         {
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null && hit.transform != lastPreviewed)
+                {
+                    Destroy(previewfurniture);
+                    previewfurniture = GameObject.Instantiate((GameObject)Resources.Load("Preview"+activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
+                } else
+                {
+                    lastPreviewed = hit.transform;
+                    previewfurniture = GameObject.Instantiate((GameObject)Resources.Load("Preview"+activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
 
             if (Physics.Raycast(ray, out hit, 100.0f))
             {
@@ -29,7 +45,8 @@ public class FurniturePlacementManager : MonoBehaviour
                     activeFurniture = "";
                 }
             }
-            
+                Destroy(previewfurniture);
+            }
         }
     }
 
