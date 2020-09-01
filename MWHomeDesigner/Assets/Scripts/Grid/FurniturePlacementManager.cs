5e4f38658807f244e0e363fc629bbd14576ca119
MrKuay <putzmaximilian@aol.com>
2020-09-01 13:38:36 +0200

    Bed is now selectable
    have to do that for other furniture

--- MWHomeDesigner/Assets/Scripts/Grid/FurniturePlacementManager.cs
+++ MWHomeDesigner/Assets/Scripts/Grid/FurniturePlacementManager.cs
@@ -23,33 +23,46 @@ public class FurniturePlacementManager : MonoBehaviour
                 if (hit.transform != null && hit.transform != lastPreviewed)
                 {
                     Destroy(previewfurniture);
-                    previewfurniture = GameObject.Instantiate((GameObject)Resources.Load("Preview"+activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
+                    previewfurniture = GameObject.Instantiate((GameObject)Resources.Load("Preview" + activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
                 } else
                 {
                     lastPreviewed = hit.transform;
-                    previewfurniture = GameObject.Instantiate((GameObject)Resources.Load("Preview"+activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
+                    previewfurniture = GameObject.Instantiate((GameObject)Resources.Load("Preview" + activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
                 }
             }
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
 
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    if (hit.transform != null)
+                    {
+                        Debug.Log("tile hit" + hit.transform.position);
+                        GameObject furniture = GameObject.Instantiate((GameObject)Resources.Load(activeFurniture), hit.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity, hit.transform);
+
+                        Debug.Log("Bed spawned");
+                        activeFurniture = "";
+                    }
+                }
+                Destroy(previewfurniture);
+            }
+        } else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f))
             {
-                if (hit.transform != null)
+                if (Input.GetKeyDown(KeyCode.Mouse0) && hit.transform.tag == "Furniture")
                 {
-                    Debug.Log("tile hit" + hit.transform.position);
-                    GameObject furniture = GameObject.Instantiate((GameObject)Resources.Load(activeFurniture), hit.transform.position + new Vector3(0,0.5f,0),Quaternion.identity, hit.transform);
-              
-                    Debug.Log("Bed spawned");
-                    activeFurniture = "";
+                    Debug.Log("selected" + hit.transform.name);
+                    Destroy(previewfurniture);
                 }
-            }
-                Destroy(previewfurniture);
+                else
+                {
+                }
             }
         }
     }
-
     public static void setActiveFurniture(string furniture)
     {
         activeFurniture = furniture;
